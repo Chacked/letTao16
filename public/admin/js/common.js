5465991f95bb7cb8a153ee155f1a8d23c156ce59
huangwei <1781795123@qq.com>
2017-11-30 10:09:29 +0800

    登录页主要第一次修改

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -8,6 +8,24 @@ $(".switch").click(function(){
 	$(".lt-left").toggleClass("now");
 	$(".lt-right .head").toggleClass("now");
 	$(".lt-right .nav").toggleClass("now");
+  $(".main").toggleClass("now");
 })
 
-// 侧边栏管理点击效果
+// 退出功能
+$(".logout").click(function() {
+	$('.modal').modal("show");
+})
+
+$("#exit").click(function(){
+	  $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      success:function(data) {
+        if(data.success){
+          //退出成功，才跳转到登录页面
+          location.href = "login.html";
+        }
+      }
+    });
+
+})
--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -17,7 +17,7 @@ $("form").bootstrapValidator({
   				max:12,
   				message:"用户名长度为6-12位"
   			},
-  			falseMessage:{
+  			callback:{
   				message:"用户名或密码不正确"
   			}
   		}
@@ -33,7 +33,7 @@ $("form").bootstrapValidator({
 	  				max:12,
 	  				message:"用户名长度为6-12位"
 	  			},
-	  			falseMessage:{
+	  			callback:{
   				message:"用户名或密码不正确"
   			}
 	  		}
@@ -50,39 +50,35 @@ $(".btn-default").click(function(){
 })
 
 // 表单校验成功发送ajax请求
-// $("form").on("success.form.bv",function(e) {
-// 	// 阻止浏览器默认跳转行为
-// 	 e.preventDefault();
-// 	// 提交登录信息
-
-
-// })
-var formData;
-function load() {
-
+$("form").on("success.form.bv",function(e) {
+	
+	// 阻止浏览器默认跳转行为
+	 e.preventDefault();
+	 
+	// 提交登录信息
+	var formData;
+	
 	formData = $("form").serialize();
-	$.ajax({
-		url:"/employee/employeeLogin",
-		type:"post",
-		data:formData,
-		success:function(data) {
-			if (data.success) {
-				location.href = "index.html";
-			}else if(data.error === 1000 ) {
-				
-				 $("form").data('bootstrapValidator').updateStatus("username", "INVALID" , "falseMessage");
-				
-			}else {
-				 $("form").data('bootstrapValidator').updateStatus("password", "INVALID" , "falseMessage");
+		$.ajax({
+			url:"/employee/employeeLogin",
+			type:"post",
+			data:formData,
+			success:function(data) {
+				if (data.success) {
+					location.href = "index.html";
+				}else if(data.error === 1000 ) {
+					
+					 $("form").data('bootstrapValidator').updateStatus("username", "INVALID" , "callback");
+					
+				}else {
+					 $("form").data('bootstrapValidator').updateStatus("password", "INVALID" , "callback");
+				}
+
 			}
 
-		}
+		})
 
-	})
-}
-	
-$(".btn-primary").click(function() {
-	load();
 })
+	
 
 
